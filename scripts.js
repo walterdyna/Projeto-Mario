bd7e73bfcc98c3c222ecbf3e7e42abdccd016e19
walterdyna <alexdyna@gmail.com>
2024-01-25 16:26:56 -0300

    falta s

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+/* 3 - JEITOS DE CRIAR UMA VARIAVEL
+        -   VAR - / JEITO ANTIGO  -- NÃO USAR
+        -   LET - VOCÊ PODE ALTERAR O VALOR DEPOIS
+        - CONST - CONSTANTE... 
+        -----------------------------------------------
+        */
 
 
 const form = document.querySelector(".formulario-fale-conosco");
@@ -7,3 +13,20 @@ function cliqueiNoBotao() {
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
 }
+
+
+console.log (" funcionando")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
