9edfe90f4d3d640ce33fd438c832833e1ad304b1
walterdyna <alexdyna@gmail.com>
2024-01-26 09:04:47 -0300

    fazendo js

--- scripts.js
+++ scripts.js
@@ -7,11 +7,18 @@
 
 
 const form = document.querySelector(".formulario-fale-conosco");
+const mascara = document.querySelector(".mascara-formulario");
 
-
-function cliqueiNoBotao() {
+function mostarForm() {
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderForm() {
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
 }
 
 
